d102680202b344558b590c8093a03263bb902ca7
Ambaliya Dhruv <ambaliyadhruv1112@gmail.com>
2022-04-23 19:16:17 +0530

    add sorting and filtering

--- src/controller/centerData.controller.js
+++ src/controller/centerData.controller.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 
 const router = express.Router();
@@ -6,13 +7,34 @@ const Center = require("../model/centerData.model");
 
 router.get("/", async (req, res) => {
   try {
-    const centers = await Center.find().lean().exec();
+   
+    const {city,costPerDay} = req.query;
+    let query = {};
+    if (city) {
+      query.city = city;
+    }
+    let order=null
+    if(costPerDay){
+      order={costPerDay:costPerDay}
+    }
+    
+    const centers = await Center.find(query).sort(order).lean().exec();
 
     res.status(200).send(centers);
   } catch (error) {
     res.send(error.message);
   }
 });
+
+router.get("/:id",async(req,res)=>{
+  try {
+     
+    const center = await Center.findById(req.params.id).lean().exec();
+    res.status(200).send(center);
+  } catch (error) {
+    res.send(error.message);
+  }
+})
 
 router.post("/", async (req, res) => {
   try {
